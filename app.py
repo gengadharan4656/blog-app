0b039f33e1a812294b3b964a79e63efa27157d04
gengadharan4656 <gengadharan4656s@gmail.com>
2025-07-05 19:40:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ import os
 import json
 from werkzeug.utils import secure_filename
 
+from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://blog-app-9a745.web.app", "*"])  # Add your web.app domain here
 
 UPLOAD_FOLDER = 'uploads'
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
