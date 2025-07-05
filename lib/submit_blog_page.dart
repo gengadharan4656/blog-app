d8c0d0ed5c5f4ec05940014d253a9690551c12ad
gengadharan4656 <gengadharan4656s@gmail.com>
2025-07-05 17:08:13 +0530

    Update submit_blog_page.dart

--- lib/submit_blog_page.dart
+++ lib/submit_blog_page.dart
@@ -57,7 +57,8 @@ class _SubmitBlogPageState extends State<SubmitBlogPage> {
       return;
     }
 
-    final uri = Uri.parse('https://your-backend-url.up.railway.app/submit_blog');
+    final uri = Uri.parse('https://blog-app-k878.onrender.com/submit_blog');
+
     final request = http.MultipartRequest('POST', uri);
     request.fields['title'] = title;
     request.fields['content'] = content;
